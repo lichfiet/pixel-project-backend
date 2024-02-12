3be05710ac79ffe0bfa5351ba950e63cb03c3b8d
Trevor Lichfield <sortaminty@gmail.com>
2024-02-11 21:49:00 -0700

    updated for docker compose and dockerfile use

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,6 +1,8 @@
 import { createClient } from 'redis';
 
-const redisClient = createClient({url: `${process.env.VITE_REDIS_URL}`});
+const redisClient = createClient({
+  url: `${process.env.VITE_REDIS_URL}`,
+});
 
 const db = {
     pg: {
